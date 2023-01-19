d5e7c969a8cc5d118217574b601efe5e83ceb219
Re9Wine <asckortan@gmail.com>
2023-01-19 12:50:02 +0300

    Create IBaseRepository.cs

---
+++ GradingSystem/DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(int id);
+        Task<int> CreateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+    }
+}
