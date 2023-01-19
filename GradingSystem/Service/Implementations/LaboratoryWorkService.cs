d227f00421dc8e0902481443f82de5612419f11d
Re9Wine <asckortan@gmail.com>
2023-01-19 16:17:17 +0300

    implemented service interfaces

---
+++ GradingSystem/Service/Implementations/LaboratoryWorkService.cs
@@ -0,0 +1,40 @@
+﻿using DAL.Interfaces;
+using Domain.Entity;
+using Service.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Service.Implementations
+{
+    public class LaboratoryWorkService : ILaboratoryWorkService
+    {
+        private readonly ILaboratoryWorkRepository _repository;
+
+        public async Task<int> CreateAsync(LaboratoryWork entity)
+        {
+            return await _repository.CreateAsync(entity);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var work = await _repository.GetAsync(id);
+
+            return await _repository.DeleteAsync(work);
+        }
+
+        public async Task<IEnumerable<LaboratoryWork>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<LaboratoryWork> GetAsync(int id)
+        {
+            return await _repository.GetAsync(id);
+        }
+
+        public async Task<bool> UpdateAsync(LaboratoryWork entity)
+        {
+            return await _repository.UpdateAsync(entity);
+        }
+    }
+}
---
+++ GradingSystem/Service/Implementations/ReportService.cs
@@ -0,0 +1,45 @@
+﻿using DAL.Interfaces;
+using Domain.Entity;
+using Service.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Service.Implementations
+{
+    public class ReportService : IReportService
+    {
+        private readonly IReportRepository _repository;
+
+        public ReportService(IReportRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> CreateAsync(Report entity)
+        {
+            return await _repository.CreateAsync(entity);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var report = await _repository.GetAsync(id);
+
+            return await _repository.DeleteAsync(report);
+        }
+
+        public async Task<IEnumerable<Report>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<Report> GetAsync(int id)
+        {
+            return await _repository.GetAsync(id);
+        }
+
+        public async Task<bool> UpdateAsync(Report entity)
+        {
+            return await _repository.UpdateAsync(entity);
+        }
+    }
+}
