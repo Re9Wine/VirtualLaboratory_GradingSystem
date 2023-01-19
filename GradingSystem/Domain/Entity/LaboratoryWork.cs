f6b015b31f3adb21319342971ea322a858190685
Re9Wine <asckortan@gmail.com>
2023-01-19 12:42:21 +0300

    Create entity based on database tables

---
+++ GradingSystem/Domain/Entity/LaboratoryWork.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+#nullable disable
+
+namespace Domain.Entity
+{
+    public partial class LaboratoryWork
+    {
+        public LaboratoryWork()
+        {
+            Reports = new HashSet<Report>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string Objective { get; set; }
+        public string Test { get; set; }
+        public string SampleReport { get; set; }
+
+        public virtual ICollection<Report> Reports { get; set; }
+    }
+}
---
+++ GradingSystem/Domain/Entity/Report.cs
@@ -0,0 +1,15 @@
+﻿#nullable disable
+
+namespace Domain.Entity
+{
+    public partial class Report
+    {
+        public int Id { get; set; }
+        public int IdLaboratoryWork { get; set; }
+        public string Content { get; set; }
+        public int? TestGrade { get; set; }
+        public int? WorkGrade { get; set; }
+
+        public virtual LaboratoryWork IdLaboratoryWorkNavigation { get; set; }
+    }
+}
